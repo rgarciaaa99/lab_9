3e50ec525e71209561b99772db224ddfebe15569
alumno <alumno@VN-3I4>
2025-03-24 09:51:49 -0600

    Agregar archivos de proyecto.

---
+++ lab9/lab9.cpp
@@ -0,0 +1,28 @@
+// lab9.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int factorial(int n) {
+	if (n < 0) {
+		throw invalid_argument("Error: Factorial de un número negativo no está definido.");
+	}
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+int main() {
+	try {
+		int numero;
+		cout << "Introduce un número para calcular el factorial: ";
+		cin >> numero;
+		int resultado = factorial(numero);
+		cout << "El factorial de " << numero << " es: " << resultado << endl;
+	}
+	catch (...) {
+		cerr << "Se ha producido un error desconocido." << endl;
+	}
+	return 0;
+}
+
